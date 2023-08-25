1ac2d13e96ebac50b60507ceea34c90a6d845ad0
AnastasiaZay <86845728+AnastasiaZay@users.noreply.github.com>
2023-08-25 16:38:51 +0400

    Revert "Refactor architecture"

--- app/src/main/java/com/example/splitthebill/MainActivity.kt
+++ app/src/main/java/com/example/splitthebill/MainActivity.kt
@@ -3,31 +3,26 @@ package com.example.splitthebill
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.theme.SplitTheBillTheme
+import com.example.splitthebill.ui.theme.SplitTheBillTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SplitTheBillTheme {
+                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting()
+                    Greeting("Android")
                 }
             }
         }
@@ -35,35 +30,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting() {
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Bottom,
-            horizontalAlignment = Alignment.Start
-        ) {
-
-            Text(
-                text = "Refill your wallet",
-                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 40.sp),
-                modifier = Modifier.padding(bottom = 16.dp)
-            )
-            Text(
-                text = "Get the tools for proper budget allocation. Read news from community members",
-                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp),
-                modifier = Modifier.padding(bottom = 16.dp)
-            )
-        }
-    }
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SplitTheBillTheme {
-        Greeting()
+        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/splitthebill/ui/theme/Type.kt
+++ app/src/main/java/com/example/splitthebill/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.theme
+package com.example.splitthebill.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
