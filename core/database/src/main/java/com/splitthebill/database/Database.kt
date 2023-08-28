3a6517b9d231f8495a104725aa1d168c4dc153bf
vladNiderkwel <108333633+vladNiderkwel@users.noreply.github.com>
2023-08-28 09:48:18 +0400

    Feature (database) add database core #17
    Добавил основу для работы с бд

---
+++ core/database/src/main/java/com/splitthebill/database/Database.kt
@@ -0,0 +1,14 @@
+import android.content.Context
+import androidx.room.Room
+
+object Database {
+    private lateinit var applicationContext: Context
+
+    fun init(context: Context) {
+        applicationContext = context
+    }
+
+    private val appDatabase: AppDatabase by lazy {
+        Room.databaseBuilder(applicationContext, AppDatabase::class.java, "database.db").build()
+    }
+}
